c45cf880ae6f4fb85c8716af1930c0c5125aae95
Andrey K <redkeybox@gmail.com>
2025-06-04 01:30:25 +0300

    (001): добавить решение и проекты в GitLab проект

---
+++ Src/Core/Olp.ProcessingService.Core.Domain/Entities/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using Olp.ProcessingService.Core.Domain.Interfaces;
+
+namespace Olp.ProcessingService.Core.Domain.Entities
+{
+    public class EntityBase<TId> : IEntity<TId>
+    {
+        #region IEntity<TId> implementation
+
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public required TId Id { get; set; }
+
+        #endregion 
+    }
+}
