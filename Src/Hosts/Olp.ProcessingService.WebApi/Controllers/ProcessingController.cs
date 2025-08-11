5896af63cb892c87fe875e4e0c5bf6a23940561e
Dmitry Ponomar <mitrybox@gmail.com>
2025-08-11 13:13:34 +0300

    ProcessingController added with the empty get method

---
+++ Src/Hosts/Olp.ProcessingService.WebApi/Controllers/ProcessingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Olp.ProcessingService.Core;
+using Olp.ProcessingService.Core.Domain;
+using System.Net.Http.Json;
+
+namespace Olp.ProcessingService.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProcessingController : ControllerBase
+    {
+        //   private readonly IProcessingService _service;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public ProcessingController(
+            //       IProcessingService service,
+            IHttpClientFactory httpClientFactory,
+            IConfiguration configuration)
+        {
+            //       _service = service;
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProcessingTask>> GetById(Guid id, CancellationToken cancellationToken) //пока просто processing task)
+        {
+        }
+
+    }
+}
